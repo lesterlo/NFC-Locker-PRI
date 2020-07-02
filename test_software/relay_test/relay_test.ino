dbd26fd3d8b71a5c39d22f020f8f96e047d1bef8
Lester Lo <21245380+lesterlo@users.noreply.github.com>
2020-07-02 12:11:40 +0800

    Create relay_test.ino

---
+++ test_software/relay_test/relay_test.ino
@@ -0,0 +1,36 @@
+/**
+ * relay test.ino
+ * 
+ * Test your relay module
+ * 
+ * Program tested on Arduino Mega 2560
+ * 
+ * Modified from https://www.arduino.cc/en/Tutorial/ToneMelody?from=Tutorial.Tone 
+ * 
+ */
+
+#define RELAY_PIN 11
+#define LED_PIN LED_BUILTIN
+
+void toogle_relay(int state)
+{
+    digitalWrite(RELAY_PIN, state);
+
+    digitalWrite(LED_PIN, state);
+}
+
+void setup()
+{
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, HIGH); //Disable the RELAY_PIN
+
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+    toogle_relay(LOW);
+    delay(2000);
+    toogle_relay(HIGH);
+    delay(2000);
+}
